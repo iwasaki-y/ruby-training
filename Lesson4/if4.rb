4dd46e6b98944475e06781f34e470478908f907f
iwasaki-y <iwasaki-y@applicative.co.jp>
2014-09-29 17:06:35 +0900

    Lesson4,1,5追加

---
+++ Lesson4/if4.rb
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+  puts("整数を入力してね")
+  number = gets.to_i
+  if number > 0 then
+  	puts("正の数です!")
+  else
+  	if number < 0 then
+  		puts("負の数です!")
+  	else
+  		puts("0です!")
+  	end
+  end
